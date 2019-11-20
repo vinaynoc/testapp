7fc85697c80bc47d4a84c6319470910bdfc71bad
vinaynoc <vinay.narayana@octanner.com>
2019-11-20 14:35:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-useless-constructor */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props,context) {
+    super(props,context);
+  }
+  componentDidMount = () => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://vision.appreciatehub.com/ui/givewidget/app.bundle.js";
+    document.head.appendChild(script);
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = "https://vision.appreciatehub.com/ui/givewidget/app.bundle.css";
+    document.head.appendChild(link);
+    const script1 = document.createElement('script');
+    script.async = true;
+    script1.src="https://vision.appreciatehub.com/ui/givewidget/giveanywhere.js";
+    document.head.appendChild(script1);
+    script1.onload = () => {
+      initGiveAnywhere("#appreciate", "https://fs-prod.octanner.com/adfs/ls/IdpInitiatedSignOn.aspx?loginToRp=fed.appreciatehub.com")
+    }
+  }  
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref = {e => (this.div = e)}>
+      
     </div>
-  );
+  );  
+  }
 }
 
 export default App;
