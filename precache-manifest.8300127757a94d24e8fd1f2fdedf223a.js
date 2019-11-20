5ce37cc4c122667c43b88f63fce4fee23a89729a
vinaynoc <vinay.narayana@octanner.com>
2019-11-20 14:37:35 +0530

    Updates

---
+++ precache-manifest.8300127757a94d24e8fd1f2fdedf223a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09776dce8d89fc4d61617900a903284",
+    "url": "/vinaynoc/testapp/index.html"
+  },
+  {
+    "revision": "f8a5cb920b960a4b3b36",
+    "url": "/vinaynoc/testapp/static/css/main.dc1cf21f.chunk.css"
+  },
+  {
+    "revision": "ecf98cd7e582c7794c78",
+    "url": "/vinaynoc/testapp/static/js/2.0bc55248.chunk.js"
+  },
+  {
+    "revision": "f8a5cb920b960a4b3b36",
+    "url": "/vinaynoc/testapp/static/js/main.ab228149.chunk.js"
+  },
+  {
+    "revision": "dada9f3fc88de6df3ff6",
+    "url": "/vinaynoc/testapp/static/js/runtime-main.02f65043.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/vinaynoc/testapp/static/media/logo.25bf045c.svg"
+  }
+]);
